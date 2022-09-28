99c624927f43e7abbd79073c7c8cfa3780df2906
AbenezerM <abenezermulugeta41@gmail.com>
2022-09-27 21:19:22 -0500

    Use keyenter on the text box

--- q2/views/js/index.js
+++ q2/views/js/index.js
@@ -1,11 +1,24 @@
 $(() => {
-    $('#ask8Ball').click(() => {
-        $.ajax({
-            url: 'http://localhost:3000/8ball', 
-            type: 'GET', 
-            success: (result) => {
-                $('#question').val(result);
-            }
-        });
-    });
+    // $('#ask8Ball').click(() => {
+    //     $.ajax({
+    //         url: 'http://localhost:3000/8ball', 
+    //         type: 'GET', 
+    //         success: (result) => {
+    //             $('#question').val(result);
+    //         }
+    //     });
+    // });
+
+    $('#question').keypress(function (e) {
+        var key = e.which;
+        if(key == 13){
+            $.ajax({
+                url: 'http://localhost:3000/8ball', 
+                type: 'GET', 
+                success: (result) => {
+                    $('#question').val(result);
+                }
+            });
+        }
+      });
 });
